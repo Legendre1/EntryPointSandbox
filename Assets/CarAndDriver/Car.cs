77ef58eb742e91fd2eda6844d34af36e93c1200b
Alex Hinkle <alex.hinkle@gmail.com>
2020-04-24 13:17:23 -0600

    wheel code set

--- Assets/CarAndDriver/Car.cs
+++ Assets/CarAndDriver/Car.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Car
 {
     private Driver mDriver;
+    private Wheel[] mWheels;
+    private Engine mEngine;
 
     public void AddDriver(Driver d)
     {
         mDriver = d;
         Debug.Log("I am a car, and I now have driver: " + mDriver);
+	    
+    }
+    
+    public void AddWheel(Wheel[] w)
+    {
+        mWheels = new Wheel[4];
+	       for(int n = 0; n < 4; n++)
+		{
+			mWheels[n] = new Wheel();
+		}
+        Debug.Log("I have four wheels!");
     }
 
 }

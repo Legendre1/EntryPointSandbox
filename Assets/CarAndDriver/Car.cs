457a7482b244e4a6d8e5c813d4f45d0bad59374f
Jon <jonlgolden@gmail.com>
2020-04-24 13:18:44 -0600

    Example stuff I did while Alex followed along.

--- Assets/CarAndDriver/Car.cs
+++ Assets/CarAndDriver/Car.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Car
 {
     private Driver mDriver;
+    private Wheel[] mWheels;
 
     public void AddDriver(Driver d)
     {
         mDriver = d;
         Debug.Log("I am a car, and I now have driver: " + mDriver);
+
+        mWheels = new Wheel[4];
+        for(int n = 0; n < 4; n++)
+        {
+            mWheels[n] = new Wheel();
+        }
     }
 
 }
